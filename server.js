6da47f91922ddf2eb3d48364177e6f144692bc2d
talha-shabbir109 <talhashabbir109@gmail.com>
2025-10-02 20:06:21 +0500

    feat: add basic express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.json({
+		message: "Affiliate Marketing API - Develop Branch",
+		branch: "develop",
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+});
