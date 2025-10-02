69472f2707c9a7319f2a290f0cda0835bee27d75
talha-shabbir109 <talhashabbir109@gmail.com>
2025-10-03 00:54:43 +0500

    feat: add prisma setup with user model

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
+// 1. Import packages
 const express = require("express");
-const app = express();
+
+// 2. Create app
+const app = express(); //pure server ka control isi k pass hota hy = this line code have all server control
 const PORT = 3000;
 
-app.use(express.json());
+// 3. Use middleware
+app.use(express.json()); // data handle karna JSON data receive karne k liya zaroori hy
+
+// 4. Define routes
 
 app.get("/", (req, res) => {
+	//= GET request handle karega  //2// = Client se aaya data  //3//= Client ko bhejna data
 	res.json({
+		success: true,
 		message: "Affiliate Marketing API - Develop Branch",
 		branch: "develop",
+		timstamp: new DataTransfer().toISOString(),
 	});
 });
+// 5. Start server
 
+//with out this server cannot start
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
 });
